fd346ce32464442bb0cfff4a763f8a2fb9a37621
adikumar1 <56322335+adikumar1@users.noreply.github.com>
2021-10-31 14:08:45 +0530

    Update and rename cycle.cpp to graph_lca.cpp

--- graph_lca.cpp
+++ graph_lca.cpp
@@ -6,6 +6,53 @@ vector<pair <int, pair<int, int>> > p;
 
 int n,m;
 
+int timer,l;
+vector<int> tin, tout;
+vector<vector<int>> up;
+
+void dfs(int v, int p)
+{
+    tin[v] = ++timer;
+    up[v][0] = p;
+    for (int i = 1; i <= l; ++i)
+        up[v][i] = up[up[v][i-1]][i-1];
+
+    for (int u : adj[v]) {
+        if (u != p)
+            dfs(u, v);
+    }
+
+    tout[v] = ++timer;
+}
+
+bool is_ancestor(int u, int v)
+{
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+ 
+int lca(int u, int v)
+{
+    if (is_ancestor(u, v))
+        return u;
+    if (is_ancestor(v, u))
+        return v;
+    for (int i = l; i >= 0; --i) {
+        if (!is_ancestor(up[u][i], v))
+            u = up[u][i];
+    } 
+    return up[u][0];
+}
+
+void preprocess(int root) {
+    tin.resize(n+1);
+    tout.resize(n+1);
+    timer = 0;
+    l = ceil(log2(n+1));
+    up.assign(n+1, vector<int>(l + 1));
+    dfs(root, root);
+}
+
+
 
 void dijkstra(int sx){
   rep(i,0,n+1)
